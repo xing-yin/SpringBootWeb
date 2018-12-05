993bf78f89dc52ed687b100ba2504d71e7d71e74
yinxing <yinxing@dxy.cn>
2018-12-05 10:23:26 +0800

    [add]新增异步调用线程示例

--- src/main/java/com/alany/other/ThreadPoolExecutor.java
+++ src/main/java/com/alany/other/ThreadPoolExecutor.java
@@ -1,6 +1,10 @@
 package com.alany.other;
 
-import org.apache.tomcat.jni.Poll;
+import com.alany.common.http.HttpClientUtil;
+import com.alany.common.http.HttpConstants;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.concurrent.*;
@@ -9,6 +13,50 @@ import java.util.concurrent.*;
  * Created by yinxing on 2018/8/8.
  */
 public class ThreadPoolExecutor {
+
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * 异步调用上报接口
+     *
+     * @param url
+     * @param stringMap
+     */
+    public void asynchronousCall(String url, Map<String, Object> stringMap) {
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+                .setNameFormat("asynchronousCall-pool-%d").build();
+
+        //Common Thread Pool
+        ExecutorService pool = new java.util.concurrent.ThreadPoolExecutor(15, 200,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory, new java.util.concurrent.ThreadPoolExecutor.AbortPolicy());
+
+        pool.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    HttpClientUtil.doGet(url, stringMap, HttpConstants.HttpClientConfig.DEFAULT);
+                } catch (Exception e) {
+                    logger.info("http上报solr失败！");
+                }
+            }
+        });
+        pool.shutdown();//gracefully shutdown
+
+
+//        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(15);
+//        fixedThreadPool.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    HttpClientUtil.doGet(url, stringMap, HttpConstants.HttpClientConfig.DEFAULT);
+//                } catch (Exception e) {
+//                    logger.info("http上报solr失败！");
+//                }
+//            }
+//        });
+    }
+
 
     public static void main(String[] args) throws InterruptedException {
         java.util.concurrent.ThreadPoolExecutor poolExecutor = new java.util.concurrent.ThreadPoolExecutor(6, 10, 30,
@@ -19,8 +67,8 @@ public class ThreadPoolExecutor {
             public void run() {
                 try {
                     Thread.sleep(2000);
-                    System.out.println(Thread.currentThread().getName() +"run");
-                }catch (Exception e){
+                    System.out.println(Thread.currentThread().getName() + "run");
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
@@ -30,7 +78,7 @@ public class ThreadPoolExecutor {
         poolExecutor.execute(myRunnable);
         poolExecutor.execute(myRunnable);
         System.out.println("======先开3个线程======");
-        System.out.println("核心线程数:" +poolExecutor.getCorePoolSize() );
+        System.out.println("核心线程数:" + poolExecutor.getCorePoolSize());
         System.out.println("线程池数:" + poolExecutor.getPoolSize());
         System.out.println("队列任务数:" + poolExecutor.getQueue().size());
         poolExecutor.execute(myRunnable);
@@ -47,7 +95,7 @@ public class ThreadPoolExecutor {
         System.out.println("队列任务数" + poolExecutor.getQueue().size());
     }
 
-    public static void asynchronousCall(String s){
+    public static void asynchronousCall(String s) {
         //有多种选择 参见:https://blog.csdn.net/qq_20160723/article/details/78131201
         ExecutorService fixedThreadPool = Executors.newFixedThreadPool(15);
         fixedThreadPool.execute(new Runnable() {
